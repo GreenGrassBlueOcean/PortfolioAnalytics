d5a98d981aa93f6842bd3499567c90739c2e6bec
Laurens <43173895+GreenGrassBlueOcean@users.noreply.github.com>
2026-03-15 11:39:08 +0100

    fix: pass CVXR Problem as positional arg to psolve() for S7 compat
    The CI has a newer CVXR version that migrated from S4 to S7, renaming
    psolve()'s first argument from 'object' to 'problem'. Using a named
    argument caused 'argument "problem" is missing' errors. Passing it
    positionally works with both old and new CVXR versions.

--- R/solver_cvxr.R
+++ R/solver_cvxr.R
@@ -241,7 +241,7 @@ solve_cvxr <- function(R, portfolio, constraints, moments, penalty,
   cvxr_dots <- dots[cvxr_solve_args]
 
   .cvxr_solve <- function(prob, solver) {
-    do.call(CVXR::psolve, c(list(object = prob, solver = solver), cvxr_dots))
+    do.call(CVXR::psolve, c(list(prob, solver = solver), cvxr_dots))
   }
 
   if (cvxr_default) {
